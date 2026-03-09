ff5e2853bbef5edf7e63644648ffe982dfafab96
Nodir Fayzullaev <nodir.fayzullaev@ssense.com>
2026-03-09 10:08:12 -0400

    up(PCIE-6991): adding deploy script

---
+++ deployment/parameters.sh
@@ -0,0 +1,28 @@
+# Copyright 2022 Amazon Web Services, Inc
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+IDC_LOGIN_URL=https://ssense-main.awsapps.com/start
+REGION=us-west-2
+TEAM_ACCOUNT=143262402689
+ORG_MASTER_PROFILE=DevOpsAccess-528260891230
+TEAM_ACCOUNT_PROFILE=DevOpsAccess-143262402689
+TEAM_ADMIN_GROUP="AWS DevOps"
+TEAM_AUDITOR_GROUP="AWS DevOps"
+TAGS="ssense:team=devops ssense:domain=tech-platform  ssense:application=iam-identity-center-team ssense:product=iam-identity-center-team ssense:environment-type=prod ssense:sensitive-data=false ssense:owneremail=devops@ssense.com"
+CLOUDTRAIL_AUDIT_LOGS=arn:aws:cloudtrail:us-west-2:528260891230:trail/aws-controltower-BaselineCloudTrail
+SECRET_NAME=TEAM-IDC-APP
+CACHE_TTL=604800
+
+# Uncomment the next line only if you have a custom domain
+# UI_DOMAIN=portal.teamtest.online
